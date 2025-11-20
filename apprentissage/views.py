8de5576e7616e81f7aee633e23e428c269bf6097
HanenRez <hanen_rezgui@yahoo.fr>
2025-11-20 17:18:48 +0100

    Description claire des changements

--- apprentissage/views.py
+++ apprentissage/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Niveau, Animation
 
 def accueil(request):
     return render(request, 'accueil.html')
+
+
+
+def liste_niveaux(request):
+    niveaux = Niveau.objects.all().order_by('ordre')
+    return render(request, "liste_niveaux.html", {"niveaux": niveaux})
+
+def detail_niveau(request, niveau_id):
+    niveau = get_object_or_404(Niveau, id=niveau_id)
+    animations = Animation.objects.filter(niveau=niveau).order_by('titre')
+
+    return render(request, "detail_niveau.html", {
+        "niveau": niveau,
+        "animations": animations
+    })
+
--- kalilawadimna/urls.py
+++ kalilawadimna/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apprentissage import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil'),
-]
+    path('lesniveaux/', views.liste_niveaux, name="liste_niveaux"),
+    path('niveau/<int:niveau_id>/', views.detail_niveau, name="detail_niveau"),
+]  
